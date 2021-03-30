282e6955ae66b27a4199ca3f505997a7898ff6a5
AcaiBerii <69873653+AcaiBerii@users.noreply.github.com>
2021-03-30 14:01:25 -0400

    MERRY CHISTMAS

--- EggySwitch/SwitchParser.cs
+++ EggySwitch/SwitchParser.cs
@@ -4,4 +4,4 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EggySwitch { public static class Ext { public static string[] IndexStrings(this int[] array, string[] args) { List<string> indexed = new List<string>(); foreach (int aint in array) { indexed.Add(args[aint]); } return indexed.ToArray();} public static int[] IndexSwitches(this string[] toparse, string[] lookfor) { List<int> intsh = new List<int>(); foreach ((string it, Int32 i) in toparse.Select((value, i) => (value, i))) { foreach (string tolookfor in lookfor) { if (it == tolookfor) { intsh.Add(i); } } } return intsh.ToArray(); } } }
+namespace EggySwitch { public static class Ext { public static string[] IndexStrings(this string[] args, int[] array) { List<string> indexed = new List<string>(); foreach (int aint in array) { indexed.Add(args[aint]); } return indexed.ToArray();} public static int[] IndexSwitches(this string[] toparse, string[] lookfor) { List<int> intsh = new List<int>(); foreach ((string it, Int32 i) in toparse.Select((value, i) => (value, i))) { foreach (string tolookfor in lookfor) { if (it == tolookfor) { intsh.Add(i); } } } return intsh.ToArray(); } } }
