078cc14a35e9870bb2a8b678a844799c39e0049a
AcaiBerii <69873653+AcaiBerii@users.noreply.github.com>
2021-03-30 11:50:13 -0400

    Add pain

--- EggySwitch/SwitchParser.cs
+++ EggySwitch/SwitchParser.cs
@@ -4,24 +4,4 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EggySwitch
-{
-    public static class Ext
-    {
-        public static int[] IndexSwitches(this string[] toparse, string[] lookfor)
-        {
-            List<int> intsh = new List<int>();
-            foreach ((string it, Int32 i) in toparse.Select((value, i) => (value, i)))
-            {
-                foreach (string tolookfor in lookfor)
-                {
-                    if (it == tolookfor)
-                    {
-                        intsh.Add(i);
-                    }
-                }
-            }
-            return intsh.ToArray();
-        }
-    }
-}
+namespace EggySwitch { public static class Ext { public static int[] IndexSwitches(this string[] toparse, string[] lookfor) { List<int> intsh = new List<int>(); foreach ((string it, Int32 i) in toparse.Select((value, i) => (value, i))) { foreach (string tolookfor in lookfor) { if (it == tolookfor) { intsh.Add(i); } } } return intsh.ToArray(); } } }
