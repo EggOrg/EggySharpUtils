1d8e6f69608b1b2d206bb7359e64ca5ff0d9b7e3
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-03 10:41:20 -0400

    Add HTML parser

---
+++ htmsharp/InBetweens.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmsharp
+{
+    public class InBetweens
+    {
+        public struct Html
+        {
+            private static string PureHTML { get; set; }
+            public Html(string htm)
+            {
+                PureHTML = htm;
+            }
+            public static class GetInBtws
+            {
+                public static string GetInBetweens(string blockname)
+                {
+                    return PureHTML.Split(new string[] { $"<{blockname}>" }, StringSplitOptions.None)[1];
+                }
+            } 
+        }
+    }
+}
