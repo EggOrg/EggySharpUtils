50231c6910b00bf336a9642945c1a73219f392bf
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-03 14:54:39 -0400

    Add MCUtils

---
+++ MCUtils/Head.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCUtils
+{
+    public partial class Head : UserControl
+    {
+        public static string UUID { get; set; }
+        public Head()
+        {
+            InitializeComponent();
+        }
+        private void GetHead(object sender, EventArgs e)
+        {
+            if (UUID != string.Empty)
+            {
+            this.BackgroundImage = Image.FromFile($"https://crafatar.com/avatars/{UUID}");
+            }
+        }
+    }
+}
