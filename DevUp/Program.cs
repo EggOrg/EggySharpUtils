e7f2e45ca07b581d4a8b98beb45facc245d5bed0
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-03 13:40:04 -0400

    Add DevUp.
    DevUp is a package manager for cool developers :))

---
+++ DevUp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+
+namespace DevUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">---> dup <---<");
+            if (args[0] == "gradle")
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Not enough args.");
+                }
+                else
+                {
+                    Console.WriteLine("Downloading the latest Gradle distribution near you...");
+                    if (args[1] == "-o")
+                    {
+                        new WebClient().DownloadFile("https://services.gradle.org/distributions/gradle-7.0-rc-2-bin.zip", args[2]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unknown command. Commands: gradle.");
+            }
+        }
+    }
+}
