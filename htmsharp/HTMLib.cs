d6b6fe860838b55db6db111171ef9ee1375f18b2
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-03 13:13:07 -0400

    woop

---
+++ htmsharp/HTMLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmsharp
+{
+    public class HTMLib
+    {
+        public struct Html
+        {
+            public string PureHTML { get; set; }
+            public Html(string htm)
+            {
+                PureHTML = htm;
+            }
+        }
+    }
+}
---
+++ htmsharp/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static htmsharp.HTMLib;
+
+namespace htmsharp
+{
+    static class Utils
+    {
+        public static string GetInBetweens(this htmsharp.HTMLib.Html htm, string blockname)
+        {
+            return htm.PureHTML.Split(new string[] { $"{blockname}" }, StringSplitOptions.None)[1].Split('>')[1].Split('<')[0];
+        }
+    }
+}
