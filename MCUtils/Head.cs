d48a99ca57a9f7fe1ac8a972e02eca8f5597993d
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-03 20:21:35 -0400

    Read desc pls
    - Split MCUtils into versions for WPF and .NET core.
    
    - Added descriptions to MCUtils classes

--- MCUtils/Head.cs
+++ MCUtils/Head.cs
@@ -12,16 +12,27 @@ using static MCUtils.Hookins;
 
 namespace MCUtils
 {
+    /// <summary>
+    /// Head control.
+    /// </summary>
     public partial class Head : UserControl
     {
         public Head()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Initializes the head control with a UUID
+        /// </summary>
+        /// <param name="uid">MCUUID</param>
         public void Init(MCUUID uid)
         {
             this.BackgroundImage = Image.FromFile($"https://crafatar.com/avatars/{uid.UUID}");
         }
+        /// <summary>
+        /// Initializes the head control with a name.
+        /// </summary>
+        /// <param name="nme">MCNAME</param>
         public void Init(MCNAME nme)
         {
             this.BackgroundImage = Image.FromFile($"https://minotar.net/avatar/{nme.NAME}");
