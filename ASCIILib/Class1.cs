82df9afc76a211f3660eee38eb392ac7364184bc
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-05 08:37:17 -0400

    ascii api

---
+++ ASCIILib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIILib
+{
+    public class Artii
+    {
+        public async Task<string> Get(string txt)
+        {
+            return await new HttpClient().GetStringAsync($"https://artii.herokuapp.com/make?text={txt.Replace(' ', '+')}");
+        }
+    }
+}
