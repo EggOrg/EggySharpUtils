e517bc5bb2aacee054c09c9a3d53653154c4c4fe
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-06 10:31:00 -0400

    Cooooooooooool

--- IPApiSharp/Main.cs
+++ IPApiSharp/Main.cs
@@ -13,6 +13,7 @@ namespace IPApiSharp
     {
         public struct IPStr
         {
+            public string Status { get; set; }
             public string Lat { get; set; }
             public string Long { get; set; }
             public string Timezone { get; set; }
@@ -21,19 +22,19 @@ namespace IPApiSharp
             public string CountryCode { get; set; }
             public string City { get; set; }
             public string Country { get; set; }
-            public string Status { get; set; }
             public string As { get; set; }
             public string Org { get; set; }
             public string Query { get; set; }
             public string Asname { get; set; }
             public string Reverse { get; set; }
             public string District { get; set; }
-            public string Mobile { get; set; }
-            public string Hosting { get; set; }
-            public string Proxy { get; set; }
+            public bool Mobile { get; set; }
+            public bool Hosting { get; set; }
+            public bool Proxy { get; set; }
             public string RegionName { get; set; }
             public string Continent { get; set; }
             public string ContinentCode { get; set; }
+            public string Message { get; set; }
         }
     }
     public class API
@@ -45,27 +46,34 @@ namespace IPApiSharp
             JObject jsr = new JObject();
             jsr = JObject.Parse(response);
             Str.IPStr jrop = new Str.IPStr();
-            jrop.Lat = (string)jsr["lat"];
-            jrop.Long = (string)jsr["lon"];
-            jrop.Zip = (string)jsr["zip"];
-            jrop.Status = (string)jsr["status"];
-            jrop.Country = (string)jsr["country"];
-            jrop.CountryCode = (string)jsr["countryCode"];
-            jrop.As = (string)jsr["as"];
-            jrop.Org = (string)jsr["org"];
-            jrop.Isp = (string)jsr["isp"];
-            jrop.Timezone = (string)jsr["timezone"];
-            jrop.City = (string)jsr["city"];
-            jrop.Query = (string)jsr["query"];
-            jrop.Asname = (string)jsr["asname"];
-            jrop.Reverse = (string)jsr["reverse"];
-            jrop.District = (string)jsr["district"];
-            jrop.Mobile = (string)jsr["mobile"];
-            jrop.Hosting = (string)jsr["hosting"];
-            jrop.Proxy = (string)jsr["proxy"];
-            jrop.RegionName = (string)jsr["regionName"];
-            jrop.Continent = (string)jsr["continent"];
-            jrop.ContinentCode = (string)jsr["continentCode"];
+            if (jsr.Count < 21)
+            {
+                throw new Exception("Error finding all values. The IP address may be wrong.");
+            }
+            else
+            {
+                jrop.Lat = (string)jsr["lat"];
+                jrop.Long = (string)jsr["lon"];
+                jrop.Zip = (string)jsr["zip"];
+                jrop.Country = (string)jsr["country"];
+                jrop.CountryCode = (string)jsr["countryCode"];
+                jrop.As = (string)jsr["as"];
+                jrop.Org = (string)jsr["org"];
+                jrop.Isp = (string)jsr["isp"];
+                jrop.Timezone = (string)jsr["timezone"];
+                jrop.City = (string)jsr["city"];
+                jrop.Query = (string)jsr["query"];
+                jrop.Asname = (string)jsr["asname"];
+                jrop.Reverse = (string)jsr["reverse"];
+                jrop.District = (string)jsr["district"];
+                jrop.Mobile = (bool)jsr["mobile"];
+                jrop.Hosting = (bool)jsr["hosting"];
+                jrop.Proxy = (bool)jsr["proxy"];
+                jrop.RegionName = (string)jsr["regionName"];
+                jrop.Continent = (string)jsr["continent"];
+                jrop.ContinentCode = (string)jsr["continentCode"];
+                jrop.Status = (string)jsr["status"];
+            }
             return jrop;
         }
     }
