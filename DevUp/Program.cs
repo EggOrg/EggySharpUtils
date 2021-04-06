9db481e6aa8a29e7f0dde7f03fdd2c10490d71c0
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-06 12:43:12 -0400

    Add Eclipse to devup.

--- DevUp/Program.cs
+++ DevUp/Program.cs
@@ -26,9 +26,27 @@ namespace DevUp
                     }
                 }
             }
+            else if (args[0] == "eclipse")
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Not enough args.");
+                }
+                else
+                {
+                    Console.WriteLine("Downloading the latest Eclipse distribution near you...");
+                    if (args[1] == "-o")
+                    {
+                        Console.WriteLine("Downloading...");
+                        WebClient wc = new WebClient();
+                        wc.DownloadFileAsync(new Uri("https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2021-03/R/eclipse-inst-jre-win64.exe"), args[2]);
+                        Console.WriteLine("Worked!");
+                    }
+                }
+            }
             else
             {
-                Console.WriteLine("Unknown command. Commands: gradle.");
+                Console.WriteLine("Unknown command. Commands: gradle, eclipse.");
             }
         }
     }
