4f1f6f8516665b1881628a631bf6e3c1cd5c8078
Steve <studiouifxdesignersandcoders@gmail.com>
2021-04-07 08:47:26 -0400

    Add RINIT parsing engine.

---
+++ RINIT/Rini.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RINIT
+{
+    public class ParsingEngine
+    {
+        public Rini.ParsedRinitFile Parse(Rini.RinitFile rn)
+        {
+            return new Rini.ParsedRinitFile(rn.FullRinit.Replace("@RINIT[<-", string.Empty).Replace("->]", string.Empty));
+        }
+    }
+    public class Rini
+    {
+        public struct RinitFile
+        {
+            public string FullRinit { get; set; }
+            public RinitFile(string rinit)
+            {
+                FullRinit = rinit;
+            }
+        }
+        public struct ParsedRinitFile {
+            public string FullParsedRinit { get; set; }
+            public ParsedRinitFile(string parsedrinit)
+            {
+                FullParsedRinit = parsedrinit;
+            }
+        }
+    }
+    public class RiniStart
+    {
+        public void Start(Rini.ParsedRinitFile rf)
+        {
+            System.Diagnostics.Process.Start(rf.FullParsedRinit);
+        }
+    }
+}
