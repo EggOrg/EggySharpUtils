8162706c29585684871bc3ce4c16292a984e05b5
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-04-09 19:36:33 -0400

    yay

---
+++ GitSharp/main.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSharp
+{
+    public class GitClient
+    {
+        public class JSON
+        {
+            public RestClient rc = new RestClient("https://api.github.com");
+            public Str.GitJSON GetUser(string username)
+            {
+                Str.GitJSON jsg = new Str.GitJSON();
+                jsg.JSON = rc.Get(new RestRequest("/users/{username}", DataFormat.Json)).Content;
+                return jsg;
+            }
+        }
+    }
+    public class Str
+    {
+        public struct GitJSON
+        {
+            public string JSON { get; set; }
+        }
+    }
+}
